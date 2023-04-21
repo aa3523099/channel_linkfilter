82b1cca044fdfed85ca248c2a1597d0481443bfa
aa3523099 <54587340+aa3523099@users.noreply.github.com>
2023-04-21 08:05:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
+API_ID       = 9590156
+API_HASH     = "368a346bb1b206b650f2b3b37f91e237"
 BOT_TOKEN    = ""
 SESSION      = ""
-DATABASE_URI = ""
+DATABASE_URI = "mongodb+srv://mrknown:mrknown@cluster0.tnemtmz.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001884373226
 ADMIN        = 1350642629
 CHANNEL      = "@GreyMatter_Bots"
